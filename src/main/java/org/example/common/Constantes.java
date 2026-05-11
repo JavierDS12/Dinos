611f4df7527bf17b454403da65c3048f62be14b7
JavierDS12 <javier.soican@educa.madrid.org>
2026-05-11 10:57:45 +0200

    bloblo

--- src/main/java/org/example/common/Constantes.java
+++ src/main/java/org/example/common/Constantes.java
@@ -1,5 +1,20 @@
 package org.example.common;
 
 public class Constantes {
-    String[] familiasDinosaurios = {"Tyrannosauridae", "Velociraptorinae", "Dromaeosauridae", "Hadrosauridae", "Ceratopsidae", "Stegosauridae", "Ankylosauridae", "Spinosauridae", "Allosauridae", "Diplodocidae", "Brachiosauridae", "Iguanodontidae", "Pachycephalosauridae", "Abelisauridae", "Compsognathidae"};
+    final String[] FAMILIASDINOSAURIOS = {"Tyrannosauridae", "Velociraptorinae", "Dromaeosauridae", "Hadrosauridae", "Ceratopsidae", "Stegosauridae", "Ankylosauridae", "Spinosauridae", "Allosauridae", "Diplodocidae", "Brachiosauridae", "Iguanodontidae", "Pachycephalosauridae", "Abelisauridae", "Compsognathidae"};
+    final String[] TYRANNOSAURIDAE = {"Tyrannosaurus rex", "Tarbosaurus bataar", "Albertosaurus sarcophagus"};
+    final String[] VELOCIRAPTORINAE = {"Velociraptor mongoliensis", "Deinonychus antirrhopus", "Tsaagan mangas"};
+    final String[] DROMAEOSAURIDAE = {"Utahraptor ostrommaysorum", "Microraptor gui", "Dakotaraptor steini"};
+    final String[] HADROSAURIDAE = {"Parasaurolophus walkeri", "Edmontosaurus annectens", "Corythosaurus casuarius"};
+    final String[] CERATOPSIDAE = {"Triceratops horridus", "Styracosaurus albertensis", "Centrosaurus apertus"};
+    final String[] STEGOSAURIDAE = {"Stegosaurus stenops", "Kentrosaurus aethiopicus", "Tuojiangosaurus multispinus"};
+    final String[] ANKYLOSAURIDAE = {"Ankylosaurus magniventris", "Euoplocephalus tutus", "Saichania chulsanensis"};
+    final String[] SPINOSAURIDAE = {"Spinosaurus aegyptiacus", "Baryonyx walkeri", "Suchomimus tenerensis"};
+    final String[] ALLOSAURIDAE = {"Allosaurus fragilis", "Saurophaganax maximus", "Epanterias amplexus"};
+    final String[] DIPLODOCIDAE = {"Diplodocus carnegii", "Apatosaurus louisae", "Barosaurus lentus"};
+    final String[] BRACHIOSAURIDAE = {"Brachiosaurus altithorax", "Giraffatitan brancai", "Sauroposeidon proteles"};
+    final String[] IGUANODONTIDAE = {"Iguanodon bernissartensis", "Mantellisaurus atherfieldensis", "Ouranosaurus nigeriensis"};
+    final String[] PACHYCEPHALOSAURIDAE = {"Pachycephalosaurus wyomingensis", "Stygimoloch spinifer", "Dracorex hogwartsia"};
+    final String[] ABELISAURIDAE = {"Carnotaurus sastrei", "Majungasaurus crenatissimus", "Abelisaurus comahuensis"};
+    final String[] COMPSOGNATHIDAE = {"Compsognathus longipes", "Sinosauropteryx prima", "Huaxiagnathus orientalis"};
 }
